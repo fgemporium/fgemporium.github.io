966a7c86d7eb3d6249d550420589b9cb50a91550
Eugen Preglej <eugenpreglej@FER-53583.local>
2022-07-12 22:47:53 +0200

    Initial commit

--- routes/home.routes.js
+++ routes/home.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-import fetch from "node-fetch";
+const fetch = require('node-fetch');
 
 router.get('/', async function(req, res){
     let site = await fetch("https://fallguysstore.com/");
