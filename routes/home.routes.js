63017fce23574275c41829f91482d0f503db71fc
Eugen Preglej <eugenpreglej@FER-53583.local>
2022-07-12 22:55:23 +0200

    Initial commit

--- routes/home.routes.js
+++ routes/home.routes.js
@@ -6,9 +6,7 @@ router.get('/', async function(req, res){
     let site = await fetch("https://fallguysstore.com/");
     let html = await site.text();
 
-    let text = html.split("Item Shop")[1];
-    text = text.split("</p>")[0];
-    text = text.split("<p>")[1];
+    let text = html.split("Item Shop")[1].split("</p>")[0].split("<p>")[1];
 
     res.render('home', {
         linkActive: "home",
