43aac03312225fef3788dd58b0a84226ca19cb93
Eugen Preglej <eugenpreglej@FER-53583.local>
2022-07-13 14:30:11 +0200

    Initial commit

--- routes/home.routes.js
+++ routes/home.routes.js
@@ -1,24 +1,55 @@
+const { text, response } = require('express');
 const express = require('express');
 const router = express.Router();
-const fetch = require('node-fetch');
+//const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+
+
+const got = (...args) => import('got').then(({default: got}) => got(...args)); 
+const extractLinks = async (url) => {
+  try {
+    // Fetching HTML
+    const response = await got(url);
+    const html = response.body;
+
+    // Using cheerio to extract <a> tags
+    const $ = cheerio.load(html);
+
+    const linkObjects = $('a');
+    // this is a mass object, not an array
+
+    // Collect the "href" and "title" of each link and add them to an array
+    const links = [];
+    linkObjects.each((index, element) => {
+      links.push({
+        text: $(element).text(), // get the text
+        href: $(element).attr('href'), // get the href attribute
+      });
+    });
+    return links;
+    // do something else here with these links, such as writing to a file or saving them to your database
+  } catch (error) {
+    console.log(error.response.body);
+  }
+};
+
 
 router.get('/', async function(req, res){
-    let site = await fetch("https://fallguysstore.com/");
-    let html = await site.text();
+    let site = await extractLinks("https://fallguysstore.com/");
+    let skins = []
 
-    let text = html.split("Item Shop")[1];
-    if(text){
-        text = text.split("</p>")[0];
-    }
-    if(text){
-        text = text.split("<p>")[1];
-    }
-    
+    site.forEach(el => {
+        if(el.href.includes("https://fallguysstore.com/wp-content/uploads/")){
+            console.log(el.href);
+            skins.push(el.href);
+        }
+    });
+
 
     res.render('home', {
         linkActive: "home",
         user: undefined,
-        daily: text
+        skins: skins
     })
 })
 
